d813d4a1816be6f5d29c17590013896cd8d2f92b
danield9509 <danield9509@gmail.com>
2024-11-09 16:55:17 -0500

    mensaje

---
+++ index.js
@@ -0,0 +1,7 @@
+const btnToggle = document.querySelector('.navbarboton')
+btnToggle.addEventListener('click',function(){
+    console.log('clicked')
+   document.getElementById('navbar').classList.toggle('active');
+});
+    
+
